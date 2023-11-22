83e2e3a83d55b5305276935944d0ab1f98aa6f61
Tomasz Wolak <tom.wolak@outlook.com>
2023-11-22 12:56:29 +0000

    Pagination on Discover/search page

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -1,11 +1,48 @@
+import ReactPaginate from "react-paginate";
 import { RecipeCard } from "./RecipeCard";
+import { useEffect, useState } from "react";
 
 export const SearchResults = ({ results }) => {
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 12;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(results.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(results.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, results]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % results.length;
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className="flex flex-wrap gap-4 justify-evenly">
-      {results.map((result) => {
-        return <RecipeCard key={result.id} recipe={result} />;
-      })}
-    </div>
+    <>
+      <div className="flex flex-wrap gap-4 justify-evenly">
+        {currentItems.map((result) => {
+          return <RecipeCard key={result.id} recipe={result} />;
+        })}
+      </div>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="Next >"
+        previousLabel="< Previous"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={pageCount}
+        renderOnZeroPageCount={null}
+        containerClassName="text-black flex flex-row gap-1 mt-4"
+        pageClassName="btn bg-[#FE5F55] relative"
+        pageLinkClassName="absolute inset-0 flex items-center justify-center"
+        previousClassName="btn bg-[#FE5F55] relative min-w-[calc(10ch+1.5rem)]"
+        previousLinkClassName="absolute inset-0 flex items-center justify-center"
+        nextClassName="btn bg-[#FE5F55] relative min-w-[calc(6ch+1.5rem)]"
+        nextLinkClassName="absolute inset-0 flex items-center justify-center"
+        breakClassName="btn bg-[#FE5F55] aspect-square"
+      />
+    </>
   );
 };
