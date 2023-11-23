9fae13ed1e9fe3ef7a0403d3f06bfa34878f535f
Tomasz Wolak <tom.wolak@outlook.com>
2023-11-23 11:58:49 +0000

    Pagination for community page

--- src/components/community/CommunityResults.js
+++ src/components/community/CommunityResults.js
@@ -1,5 +1,7 @@
 import { useApp } from "../../context/AppProvider";
 import { CommunityCookbookCard } from "./CommunityCookbookCard";
+import ReactPaginate from "react-paginate";
+import { useState, useEffect } from "react";
 
 export const CommunityResults = ({ searchQuery }) => {
   const { state } = useApp();
@@ -10,15 +12,55 @@ export const CommunityResults = ({ searchQuery }) => {
       )
     : state.cookbooks;
 
-  console.log(filteredData);
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 9;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(filteredData.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(filteredData.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, filteredData]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % filteredData.length;
+    setItemOffset(newOffset);
+  };
+
   return (
     <>
-      {filteredData &&
-        filteredData?.map((cookbook) => {
+      {currentItems &&
+        currentItems.length > 0 &&
+        currentItems?.map((cookbook) => {
           return (
             <CommunityCookbookCard key={cookbook.id} cookbook={cookbook} />
           );
         })}
+      {currentItems && currentItems.length === 0 && (
+        <div className="text-center py-6 w-full text-error">
+          There seems to be no results :/, try adding a cookbook yourself :D
+        </div>
+      )}
+      <div className="mx-auto">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="Next >"
+          previousLabel="< Previous"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          pageCount={pageCount}
+          renderOnZeroPageCount={null}
+          containerClassName="text-black flex flex-row gap-1 mt-4"
+          pageClassName="btn bg-[#FE5F55] relative"
+          pageLinkClassName="absolute inset-0 flex items-center justify-center"
+          previousClassName="btn bg-[#FE5F55] relative min-w-[calc(10ch+1.5rem)]"
+          previousLinkClassName="absolute inset-0 flex items-center justify-center"
+          nextClassName="btn bg-[#FE5F55] relative min-w-[calc(6ch+1.5rem)]"
+          nextLinkClassName="absolute inset-0 flex items-center justify-center"
+          breakClassName="btn bg-[#FE5F55] aspect-square"
+        />
+      </div>
     </>
   );
 };
